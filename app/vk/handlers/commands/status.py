90b740409bea054f7d49c3c5775e5d226c001501
eledays <leotol1432@gmail.com>
2026-07-03 19:28:26 +0400

    feat: cookies check for status message

--- app/vk/handlers/commands/status.py
+++ app/vk/handlers/commands/status.py
@@ -1,5 +1,7 @@
 from app.utils.logging import log_message_handling
 from app.services.strava import StravaClient
+from app.services.users import get_or_create_user_by_vk_id
+from app.services.cookie import parse_cookies_from_string
 
 from logging import getLogger
 
@@ -11,24 +13,60 @@ def handle_status_message(bot, message: dict):
     user_id = message["from_id"]
 
     log_message_handling(logger, __name__, user_id, text)
+    user = get_or_create_user_by_vk_id(user_id)
 
-    bot.send_message(
+    sent_msg_id = bot.send_message(
         user_id=user_id,
-        message="Проверяю доступность strava",
+        message="Проверяю доступность strava...",
     )
 
-    client = StravaClient()
+    client = StravaClient(parse_cookies_from_string(user.strava_cookies))
     is_available, error = client.check_access()
 
-    if is_available:
+    has_cookies = user.strava_cookies is not None
+        
+    bot.delete_message(
+        peer_id=message["peer_id"],
+        message_id=sent_msg_id
+    )
+
+    if not is_available and has_cookies:
         bot.send_message(
             user_id=user_id,
-            message="✅ Strava доступна",
+            message=f"🚫 Strava недоступна: {error}\n✅ Cookies установлены\n🚫 Проверить валидность cookies не удалось",
+        )
+    elif not is_available and not has_cookies:
+        bot.send_message(
+            user_id=user_id,
+            message=f"🚫 Strava недоступна: {error}\n🚫 Cookies не установлены",
+        )
+    elif is_available and not has_cookies:
+        bot.send_message(
+            user_id=user_id,
+            message=f"✅ Strava доступна\n🚫 Cookies не установлены",
         )
     else:
-        bot.send_message(
+        sent_msg_id = bot.send_message(
             user_id=user_id,
-            message=f"🚫 Strava недоступна: {error}",
+            message=f"✅ Strava доступна\n✅ Cookies установлены\nПроверяю валидность cookies...",
         )
+
+        is_valid, error = client.check_cookies()
+        
+        bot.delete_message(
+            peer_id=message["peer_id"],
+            message_id=sent_msg_id
+        )
+
+        if not is_valid:
+            bot.send_message(
+                user_id=user_id,
+                message=f"✅ Strava доступна\n✅ Cookies установлены\n🚫 Cookies не валидны",
+            )
+        else:
+            bot.send_message(
+                user_id=user_id,
+                message=f"✅ Strava доступна\n✅ Cookies установлены\n✅ Cookies валидны",
+            )
 
 
